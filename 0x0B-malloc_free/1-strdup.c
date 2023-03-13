4217ee0d70d0ac365a9668b39306dda6658a4b90
accie <accienjenga@gmail.com>
2023-03-13 09:10:17 -0700

    one

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,20 +13,24 @@
 
 char *_strdup(char *str)
 {
-	char *new_string = malloc(sizeof(char) * (strlen(str) + 1));
-	int i = 0;
+	char *duplicate;
+	int index, len = 0;
 
-	if (str == 0 || new_string == NULL || str == NULL)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
-	else
-	{
-		while (str[i] != '\0')
-		{
-			new_string[i] = str[i];
-			i++;
-		}
-	}
-	return (new_string);
+
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
